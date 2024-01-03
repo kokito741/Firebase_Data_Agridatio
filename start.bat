ba18da4632ffc75572aa81c371e8c25c10cae6b5
kokito741 <48101187+kokito741@users.noreply.github.com>
2024-01-03 12:14:22 +0200

    fix bat file
    Signed-off-by: kokito741 <48101187+kokito741@users.noreply.github.com>

--- start.bat
+++ start.bat
@@ -1,6 +1,11 @@
-
-
-
+# Activate your virtual environment
 call venv\Scripts\activate.bat
 
+# Start the first Python script
 python main.py
+
+# Wait for 1 second
+timeout /t 1 /nobreak
+
+# Start the second Python script in a new terminal
+start cmd /k python openobserve_exporter.py
