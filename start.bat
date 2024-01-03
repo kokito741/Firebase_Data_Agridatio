51bb8004000329b617ce8ce65e3a8b4d96ed4a68
kokito741 <48101187+kokito741@users.noreply.github.com>
2024-01-03 12:58:53 +0200

    fix background
    Signed-off-by: kokito741 <48101187+kokito741@users.noreply.github.com>

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 # Activate your virtual environment
 call venv\Scripts\activate.bat
 # Start the second Python script in a new terminal
-start cmd /k python openobserve_exporter.py
+start cmd /k pythonw.exe openobserve_exporter.py
 
 # Start the first Python script
 python main.py
